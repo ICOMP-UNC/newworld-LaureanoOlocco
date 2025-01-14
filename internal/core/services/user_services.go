f6782fa1dc36268342eb33b3fa5537be0d9bc055
LaureanoOlocco <laureanoolocco@gmail.com>
2025-01-13 23:44:00 -0300

    refactor: update user services and handlers

--- internal/core/services/user_services.go
+++ internal/core/services/user_services.go
@@ -1,15 +1,50 @@
 package services
 
 import (
+	"errors"
+	"log"
+
 	"github.com/ICOMP-UNC/newworld-LaureanoOlocco/internal/core/domain"
-	"github.com/ICOMP-UNC/newworld-LaureanoOlocco/internal/repositories"
+	"github.com/ICOMP-UNC/newworld-LaureanoOlocco/internal/core/ports"
 )
 
-// AddUser adds a new user to the database
-func CreateUser(register domain.Register) error {
-	return repositories.AddUser(register)
+type UserService struct {
+	userRepository ports.IUserRepository
 }
 
-func LoginUser(login domain.Login) (string, error) {
-	return repositories.Login(login)
+// Esta línea es para obtener feedback en caso de que no estemos implementando la interfaz correctamente
+var _ ports.IUserService = (*UserService)(nil)
+
+func NewUserService(repository ports.IUserRepository) *UserService {
+	return &UserService{
+		userRepository: repository,
+	}
+}
+
+func (s *UserService) Login(email string, password string) error {
+	err := s.userRepository.Login(email, password)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *UserService) Register(username, email, password, confirmPass string) error {
+	if password != confirmPass {
+		return errors.New("the passwords are not equal")
+	}
+	err := s.userRepository.Register(username, email, password)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	return nil
+}
+
+func (s *UserService) GetUserByEmail(email string) (*domain.User, error) {
+	user, err := s.userRepository.GetUserByEmail(email)
+	if err != nil {
+		return nil, err
+	}
+	return user, nil
 }
--- internal/handlers/user_handlers.go
+++ internal/handlers/user_handlers.go
@@ -1,57 +1,124 @@
 package handlers
 
 import (
-	"github.com/ICOMP-UNC/newworld-LaureanoOlocco/internal/core/domain"
-	"github.com/ICOMP-UNC/newworld-LaureanoOlocco/internal/core/services"
+	"log"
 
+	"github.com/ICOMP-UNC/newworld-LaureanoOlocco/internal/core/domain"
+	"github.com/ICOMP-UNC/newworld-LaureanoOlocco/internal/core/ports"
+	"github.com/ICOMP-UNC/newworld-LaureanoOlocco/internal/utils"
 	"github.com/gofiber/fiber/v2"
 )
 
-// Register godoc
-// @Summary Register
-// @Description register a new user
-// @Tags auth
-// @Param Register body domain.Register true "User registration details"
-// @Accept  json
-// @Produce  json
-// @Success 201 {string} string "User added"
-// @Failure 500 {string} string "Bad server"
-// @Failure 400 {string} string "Bad request"
-// @Router /auth/register [post]
-func Register(c *fiber.Ctx) error {
-	var register domain.Register
-	if err := c.BodyParser(&register); err != nil {
-		return c.Status(400).SendString("Bad request")
-	}
-
-	if err := services.CreateUser(register); err != nil {
-		return c.Status(500).SendString("Bad server")
-	}
-
-	return c.Status(201).SendString("User added")
+// UserHandlers is a struct to represent the user handlers
+type UserHandlers struct {
+	userService ports.IUserService
 }
 
-// Login godoc
-// @Summary Login
-// @Description login a user
-// @Tags auth
+// NewUserHandlers creates a new UserHandlers instance with the provided user service
+func NewUserHandlers(service ports.IUserService) *UserHandlers {
+	return &UserHandlers{userService: service}
+}
+
+// @Summary User login
+// @Description Login with email and password
+// @Tags Users
 // @Accept  json
 // @Produce  json
-// @Param Login body domain.Login true "User login details"
-// @Success 200 {string} string "JWT"
-// @Failure 500 {string} string "Bad server"
-// @Failure 400 {string} string "Bad request"
-// @Router /auth/login [post]
-func Login(c *fiber.Ctx) error {
-	var login domain.Login
-	if err := c.BodyParser(&login); err != nil {
-		return c.Status(400).SendString("Bad request")
+// @Param   userLogin     body    domain.UserLogin     true        "User login details"
+// @Success 200 {object} domain.UserResponseLogin
+// @Response 400 {object} domain.BadResponse
+// @Response 404 "User not found"
+// @Response 401 "Invalid password"
+// @Response 500 "Bad server"
+// @Router /user/login [post]
+func (h *UserHandlers) Login(c *fiber.Ctx) error {
+	var req domain.UserLogin
+
+	// Parse the body into the req struct
+	if err := c.BodyParser(&req); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(domain.BadResponse{Code: "400", Message: "Bad request"})
 	}
 
-	token, err := services.LoginUser(login)
+	// if the request is fine, we call the login function from the user service
+	err := h.userService.Login(req.Email, req.Password)
 	if err != nil {
-		return c.Status(500).SendString("Bad server")
+		if err.Error() == "user not found" {
+			return c.Status(fiber.StatusNotFound).JSON(domain.BadResponse{Code: "404", Message: "User not found"})
+		}
+		if err.Error() == "invalid password" {
+			return c.Status(fiber.StatusUnauthorized).JSON(domain.BadResponse{Code: "401", Message: "Invalid password"})
+		}
+		return c.Status(fiber.StatusInternalServerError).JSON(domain.BadResponse{Code: "500", Message: "Bad server"})
 	}
 
-	return c.SendString(token)
+	// if the login is successful, we generate a token
+	token, err := utils.GenerateJWT(req.Email, req.Password)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(domain.BadResponse{Code: "500", Message: "Bad server"})
+	}
+
+	// if the token is generated successfully, we return the token
+	return c.JSON(domain.UserResponseLogin{Code: "200", Token: token})
+}
+
+// @Summary User registration
+// @Description Register with username, email, password, and confirm password
+// @Tags Users
+// @Accept  json
+// @Produce  json
+// @Param   userRegister    body    domain.UserRegister     true        "User registration details"
+// @Router /user/register [post]
+// @Success 201 {object} domain.Response
+// @Response 400 {object} domain.BadResponse
+// @Response 401 "The passwords are not equal"
+// @Response 402 "Mail already registered"
+// @Response 500 "Bad server"
+func (h *UserHandlers) Register(c *fiber.Ctx) error {
+	var req domain.UserRegister
+	if err := c.BodyParser(&req); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(domain.BadResponse{Code: "400", Message: "Bad request"})
+	}
+
+	err := h.userService.Register(req.Username, req.Email, req.Password, req.ConfirmPassword)
+	if err != nil {
+		log.Println(err)
+		if err.Error() == "the passwords are not equal" {
+			return c.Status(fiber.StatusBadRequest).JSON(domain.BadResponse{Code: "401", Message: err.Error()})
+		} else if err.Error() == "email already registered" {
+			return c.Status(fiber.StatusBadRequest).JSON(domain.BadResponse{Code: "402", Message: err.Error()})
+		}
+		return c.Status(fiber.StatusInternalServerError).JSON(domain.BadResponse{Code: "500", Message: "Bad server"})
+	}
+
+	return c.Status(fiber.StatusCreated).JSON(domain.Response{Code: "201", Message: "User added"})
+}
+
+// @Summary Get user by email
+// @Description Get user details by email
+// @Tags Users
+// @Accept  json
+// @Produce  json
+// @Param   email     query    string     true        "Email"
+// @Success 200 {object} domain.User
+// @Response 404 "Not found"
+// @Response 500 "Bad server"
+// @Router /user/search [get]
+func (h *UserHandlers) GetUserByEmail(c *fiber.Ctx) error {
+	email := c.Query("email")
+
+	user, err := h.userService.GetUserByEmail(email)
+
+	if err != nil {
+		return c.JSON(fiber.Map{"error": err.Error()})
+	}
+
+	if user == nil {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{"error": "user not found"})
+	}
+
+	return c.JSON(domain.User{
+		ID:       user.ID,
+		Email:    user.Email,
+		Password: user.Password,
+	})
 }
